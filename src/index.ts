badb4463d2b1091907f172f2f62a45d72a9b4811
Nisakoo <modeniki10@gmail.com>
2026-06-02 19:11:39 +0300

    fix: rebase

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import type { Request, Response } from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import loggerMiddleware from "./middleware/logger.js";
 import servicesRouter from "./routes/services.js";
 import { init } from "./services/servicesService.js";
 import type { ErrorRespond } from "./common/response.js";
 import errorMiddleware from "./middleware/error.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT: number = 3000;
 
 // Init servicesService
-const DATA_FILE_PATH = path.join(import.meta.dirname, "data/services.json");
+const DATA_FILE_PATH = path.join(__dirname, "data/services.json");
 init(DATA_FILE_PATH);
 
 // Middleware
--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,17 +1,16 @@
 import fs from "fs";
-import type { Stock } from "../types/stock.js";
 
 const readData = (path: string) => {
   try {
     const data = fs.readFileSync(path, "utf-8");
-    return JSON.parse(data) as Stock[];
+    return JSON.parse(data);
   } catch (err) {
     console.log("fs.readFileSync:", err);
     return [];
   }
 };
 
-const writeData = (path: string, data: Stock[]) => {
+const writeData = (path: string, data: unknown) => {
   try {
     fs.writeFileSync(path, JSON.stringify(data, null, 2), "utf-8");
   } catch (err) {
